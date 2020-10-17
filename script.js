72c91150db24d22e7f9efd6a78ca02e9ae8b217a
James <JamesDLowe@protonmail.com>
2020-10-17 17:06:26 +0100

    Hover trail complete

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const container = document.getElementById("container");
 for (i = 0; i < 256; i++) {
     container.innerHTML += '<div class="box"></div>'
 }
+
+document.querySelectorAll(".box").forEach(item => {
+    item.addEventListener("mouseover", event => {
+        item.style.backgroundColor = "blue";
+        console.log("cheese");
+    })
+})
