d3d4981a25655e03df93d1c8d6337b16734c88e4
James <JamesDLowe@protonmail.com>
2020-10-17 18:36:33 +0100

    Changed grid creation system to work better

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ for (i = 0; i < 256; i++) {
     container.innerHTML += '<div class="box"></div>'
 }
 
+function createGrid(x, y) {
+    container.innerHTML = "";
+    container.style.gridTemplateColumns = `repeat(${x}, auto)`;
+    container.style.gridTemplateRows = `repeat(${y}, auto)`;
+    for(let i = 0; i < x; i++) {
+        for(let j = 0; j < y; j++) {
+            let createBox = document.createElement("div");
+            createBox.setAttribute("class", "box");
+            container.appendChild(createBox);
+        }
+    }
+}
+
 document.querySelectorAll(".box").forEach(item => {
     item.addEventListener("mouseover", event => {
         item.style.backgroundColor = "black";
@@ -11,7 +24,7 @@ document.querySelectorAll(".box").forEach(item => {
     })
 })
 
-function createGrid () {
+/* function createGrid () {
     deleteBoxes();
     let x = prompt("How many columns(x axis)?", "20");
     let y = prompt("How many rows (y axis)?", "20");
@@ -23,7 +36,7 @@ function createGrid () {
     container.style.gridTemplateRows = `repeat(${y}, auto)`;
     addListeners();
 }
-
+*/
 function deleteBoxes () {
     document.querySelectorAll(".box").forEach(item => {
         item.remove();
@@ -34,7 +47,16 @@ function addListeners () {
     document.querySelectorAll(".box").forEach(item => {
         item.addEventListener("mouseover", event => {
             item.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-            console.log("cheese");
         })
     })
 }
+
+function newGrid() {
+    let x = prompt("How many columns(x axis)?", "20");
+    let y = prompt("How many rows (y axis)?", "20");
+    createGrid(x, y);
+    addListeners();
+}
+
+createGrid(16, 16);
+addListeners();
