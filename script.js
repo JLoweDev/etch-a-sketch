d674d3ceaf9a246630f9d6c28ef111107d66bfe7
James <JamesDLowe@protonmail.com>
2020-10-17 18:38:37 +0100

    cleaned up js file

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const container = document.getElementById("container");
 
-for (i = 0; i < 256; i++) {
-    container.innerHTML += '<div class="box"></div>'
-}
 
 function createGrid(x, y) {
     container.innerHTML = "";
@@ -15,32 +12,6 @@ function createGrid(x, y) {
             container.appendChild(createBox);
         }
     }
-}
-
-document.querySelectorAll(".box").forEach(item => {
-    item.addEventListener("mouseover", event => {
-        item.style.backgroundColor = "black";
-        console.log("cheese");
-    })
-})
-
-/* function createGrid () {
-    deleteBoxes();
-    let x = prompt("How many columns(x axis)?", "20");
-    let y = prompt("How many rows (y axis)?", "20");
-    let z = x * y;
-    for (i = 0; i < z; i++) {
-        container.innerHTML += '<div class="box"></div>'
-    }
-    container.style.gridTemplateColumns = `repeat(${x}, auto)`;
-    container.style.gridTemplateRows = `repeat(${y}, auto)`;
-    addListeners();
-}
-*/
-function deleteBoxes () {
-    document.querySelectorAll(".box").forEach(item => {
-        item.remove();
-        })
 }
 
 function addListeners () {
